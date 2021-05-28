8c32288b6c54885727d1731a09db769e17ed8dcd
GuyKoZ <63003121+GuyKoZ@users.noreply.github.com>
2021-05-29 00:52:03 +0300

    Update main.java

--- cinemaM/main.java
+++ cinemaM/main.java
@@ -17,7 +17,43 @@ package cinemaM;
 public class main {
 
     public static void main(String[] args) {
-
+        int[] totalHours = new int[30];
+//        Cinema Oby = new Cinema("The Pushtak Cinema", 6, 2, 2, 2);
+//        LocalDate releaseDate = LocalDate.of(2022, 10, 9);
+//        LocalDate graduationDate = LocalDate.of(2023, 2, 17);
+//        Customer C1 = new Soldier(1, 40, 19, "Omer", "Omer@mail", releaseDate);
+//        Customer C2 = new Student(2, 42, 23, "Yaakov", "Yaakov@mail", graduationDate, "MTA");
+//        Customer C3 = new Pensioner(3, 43, 82, "Yehezkel", "Yehezkel@mail");
+//        Customer C4 = new Customer(4, 45, 42, "David", "David@mail");
+//        Oby.addNewCustomer(C1);
+//        Oby.addNewCustomer(C2);
+//        Oby.addNewCustomer(C3);
+//        Oby.addNewCustomer(C4);
+//        Employee E1 = new Employee(123, 339, 33, "Tal", "Tal@mail", totalHours, 40);
+//        Employee E2 = new Employee(124, 2725, 22, "Or", "Or@mail", totalHours, 35);
+//        Oby.addNewEmployee(E1);
+//        Oby.addNewEmployee(E2);
+//        Movie M1 = new ThrillerMovie("The Godfather", 16, 140);
+//        Movie M2 = new ThrillerMovie("The Zodiac", 16, 155);
+//        Movie M3 = new ComedyMovie("Charli And a Half", 0, 110);
+//        Movie M4 = new ComedyMovie("Charli And a One", 0, 90);
+//        Movie M5 = new HorrorMovie("The Saw", 18, 100);
+//        Movie M6 = new HorrorMovie("The Champions", 18, 115);
+//        Oby.addNewMovie(M1);
+//        Oby.addNewMovie(M2);
+//        Oby.addNewMovie(M3);
+//        Oby.addNewMovie(M4);
+//        Oby.addNewMovie(M5);
+//        Oby.addNewMovie(M6);
+//        System.out.println(Oby.toString());
+//        Oby.sellMovieTicket();
+//        Oby.sellMovieTicket();
+//        Oby.sellMovieTicket();
+//        Oby.sellPopcorn();
+//        Oby.sellPopcorn();
+//        Oby.sellPopcorn();
+//        Movie M7 = new ComedyMovie("Party in the Pool", 0, 95);
+//        Oby.cinemaUpdateMovie("The Godfather", M7);
     }
 }
 
